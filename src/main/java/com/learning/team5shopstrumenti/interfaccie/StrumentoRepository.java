b479baabf70f7fa10b290cf867c910682d39c1ef
Gabriele02 <gagag@g,ail.com>
2024-01-24 14:59:41 +0100

    Aggiunta funzione di ricerca via marca

--- src/main/java/com/learning/team5shopstrumenti/interfaccie/StrumentoRepository.java
+++ src/main/java/com/learning/team5shopstrumenti/interfaccie/StrumentoRepository.java
@@ -3,7 +3,9 @@ package com.learning.team5shopstrumenti.interfaccie;
 import com.learning.team5shopstrumenti.model.Strumento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StrumentoRepository extends JpaRepository<Strumento, Integer> {
+import java.util.List;
 
+public interface StrumentoRepository extends JpaRepository<Strumento, Integer> {
+    List<Strumento> findByMarcaContaining (String search);
 
 }
