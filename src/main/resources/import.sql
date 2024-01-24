62b2d6aadb57b8ad37e3f299fce69847ab5397e7
Gabriele02 <gagag@g,ail.com>
2024-01-24 11:48:57 +0100

    Add import sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,78 @@
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Eko', 'Chitarra classica', 123.45, 'photo.jpg', 'Chitarra classica');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Eko', 'Chitarra acustica', 156.78, 'photo2.jpg', 'Chitarra acustica');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Chitarra elettrica', 189.01, 'photo3.jpg', 'Chitarra elettrica');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Chitarra classica', 221.23, 'photo4.jpg', 'Chitarra classica');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Eko', 'Chitarra classica', 253.45, 'photo5.jpg', 'Chitarra classica');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Chitarra elettrica', 285.67, 'photo6.jpg', 'Chitarra elettrica');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Eko', 'Chitarra elettrica', 317.89, 'photo7.jpg', 'Chitarra elettrica');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Chitarra elettrica', 350.12, 'photo8.jpg', 'Chitarra elettrica');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Chitarra acustica', 382.34, 'photo9.jpg', 'Chitarra acustica');
+
+-- Tastiere
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Numa', 'Tastiera', 123.45, 'photo10.jpg', 'Tastiera 88 tasti pesati');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Tastiera', 156.78, 'photo11.jpg', 'Tastiera 61 tasti semipesati');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Numa', 'Tastiera', 189.01, 'photo12.jpg', 'Tastiera 76 tasti semipesati');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Tastiera', 221.23, 'photo13.jpg', 'Tastiera 49 tasti semipesati');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Numa', 'Tastiera', 253.45, 'photo14.jpg', 'Tastiera 61 tasti leggeri');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Tastiera', 285.67, 'photo15.jpg', 'Tastiera 76 tasti leggeri');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Numa', 'Tastiera', 317.89, 'photo16.jpg', 'Tastiera 49 tasti leggeri');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Tastiera', 350.12, 'photo17.jpg', 'Tastiera 61 tasti sensibili alla dinamica');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Numa', 'Tastiera', 382.34, 'photo18.jpg', 'Tastiera 76 tasti sensibili alla dinamica');
+
+-- Batterie
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Pearl', 'Batteria', 123.45, 'photo19.jpg', 'Batteria acustica a 5 pezzi');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Batteria', 156.78, 'photo20.jpg', 'Batteria acustica a 7 pezzi');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Pearl', 'Batteria', 189.01, 'photo21.jpg', 'Batteria acustica a 9 pezzi');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Pearl', 'Batteria', 221.23, 'photo22.jpg', 'Batteria elettronica a 5 pezzi');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Batteria', 253.45, 'photo23.jpg', 'Batteria elettronica a 7 pezzi');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Batteria', 285.67, 'photo24.jpg', 'Batteria elettronica a 9 pezzi');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Pearl', 'Batteria', 317.89, 'photo25.jpg', 'Batteria elettronica portatile');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Batteria', 350.12, 'photo26.jpg', 'Batteria acustica a rullante singolo');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Pearl', 'Batteria', 382.34, 'photo27.jpg', 'Batteria acustica a rullante doppio');
+
+
+--Bassi
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Fender', 'Basso', 123.45, 'photo19.jpg', 'Basso acustico');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Basso', 156.78, 'photo20.jpg', 'Basso acustico');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Fender', 'Basso', 189.01, 'photo21.jpg', 'Basso eletrico');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Basso', 221.23, 'photo22.jpg', 'Basso acustico');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Fender', 'Basso', 253.45, 'photo23.jpg', 'Basso eletrico');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Basso', 285.67, 'photo24.jpg', 'Basso acustico');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Fender', 'Basso', 317.89, 'photo25.jpg', 'Basso eletrico');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('YAMAHA', 'Basso', 350.12, 'photo26.jpg', 'Basso eletrico');
+
+INSERT INTO strumenti (marca, modello, prezzo, foto, descrizione)VALUES ('Fender', 'Basso', 382.34, 'photo27.jpg', 'Basso acustico');
+
