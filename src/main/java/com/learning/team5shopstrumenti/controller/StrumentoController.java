6a254d5ac4afab4ab2eef9fee83e5c534b033024
LuigiantonioIacomino <iacominoluigi54@gmail.com>
2024-01-24 12:11:31 +0100

    .

--- src/main/java/com/learning/team5shopstrumenti/controller/StrumentoController.java
+++ src/main/java/com/learning/team5shopstrumenti/controller/StrumentoController.java
@@ -4,10 +4,13 @@ import com.learning.team5shopstrumenti.interfaccie.StrumentoRepository;
 import com.learning.team5shopstrumenti.model.Strumento;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/strumenti")
@@ -17,9 +20,16 @@ public class StrumentoController {
     private StrumentoRepository strumentoRepository;
 
     @GetMapping
-    public String index() {
+    public String index(Model model) {
         // Listaa strumenti
         List<Strumento> strumenti = strumentoRepository.findAll();
+        model.addAttribute("strumenti",strumenti);
         return "strumenti/list";
+    }
+
+
+    @GetMapping("/edit")
+    public String edit(@PathVariable Integer id, Model model) {
+        Optional<Strumento> result=strumentoRepository.findById(id);
     }
 }
--- src/main/java/com/learning/team5shopstrumenti/interfaccie/StrumentoRepository.java
+++ src/main/java/com/learning/team5shopstrumenti/interfaccie/StrumentoRepository.java
@@ -4,4 +4,6 @@ import com.learning.team5shopstrumenti.model.Strumento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StrumentoRepository extends JpaRepository<Strumento, Integer> {
+
+
 }
