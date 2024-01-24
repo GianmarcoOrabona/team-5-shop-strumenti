d80ffab9275c4c0ec320dd97aa386215e75b97aa
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2024-01-24 10:23:04 +0100

    Initial commit

---
+++ src/main/java/com/learning/team5shopstrumenti/Team5ShopStrumentiApplication.java
@@ -0,0 +1,13 @@
+package com.learning.team5shopstrumenti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team5ShopStrumentiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team5ShopStrumentiApplication.class, args);
+	}
+
+}
