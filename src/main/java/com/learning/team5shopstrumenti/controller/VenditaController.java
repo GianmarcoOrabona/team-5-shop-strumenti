18775513dbca05cfec9a5804c946851a3455e966
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2024-01-26 10:48:09 +0100

    vendita controller

---
+++ src/main/java/com/learning/team5shopstrumenti/controller/VenditaController.java
@@ -0,0 +1,24 @@
+package com.learning.team5shopstrumenti.controller;
+
+import com.learning.team5shopstrumenti.interfaccie.VenditaRepository;
+import com.learning.team5shopstrumenti.model.Vendita;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vendite")
+public class VenditaController {
+
+    @Autowired
+    VenditaRepository venditaRepository;
+
+    @PostMapping
+    public String store(@ModelAttribute("vendita")Vendita vendita, Model model) {
+        Vendita savedVendita = venditaRepository.save(vendita);
+        return "redirect:/strumenti";
+    }
+}
