2f3b678ac13b29b19d06e72f77be6def0b344676
Gabriele02 <gagag@g,ail.com>
2024-01-29 13:10:35 +0100

    Lavoro sulla pagina di check out

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,41 +1,4 @@
-const url = window.location.pathname;
-const addButton = document.getElementById('addButton');
-
-    if (url === '/admin/strumenti') {
-        addButton.style.display = 'block';
-    } else {
-        addButton.style.display = 'none';
-    }
-    const editButton = document.getElementById('editButton');
-
-        if (url === '/admin/strumenti') {
-            editButton.style.display = 'block';
-        } else {
-            editButton.style.display = 'none';
-        }
-
-    let deliteButton = document.getElementById('deliteButton');
-
-        if (url === '/admin/strumenti') {
-            deliteButton.style.display = 'block';
-        } else {
-            deliteButton.style.display = 'none';
-        }
-
-
-    let buyButton = document.getElementById('buyButton');
-
-    if (url === '/strumenti') {
-      buyButton.style.display = 'block';
-    } else {
-      buyButton.style.display = 'none';
-                }
-
-    function enableButton(buttonId, link) {
-        const url = window.location.pathname;
-         if (url === link) {
-                buttonId.style.display = 'block';
-            } else {
-                buttonId.style.display = 'none';
-            }
-    }
+document.getElementById('liveToastBtn').addEventListener('click', function() {
+  var toast = new bootstrap.Toast(document.getElementById('liveToast'))
+  toast.show()
+})
