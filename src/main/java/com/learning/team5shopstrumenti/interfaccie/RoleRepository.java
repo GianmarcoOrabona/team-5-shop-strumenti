398240abd846f28a34c62adc09bcc005138aa8e3
Gabriele02 <gagag@g,ail.com>
2024-01-31 16:56:12 +0100

    import sql

---
+++ src/main/java/com/learning/team5shopstrumenti/interfaccie/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.learning.team5shopstrumenti.interfaccie;
+
+import com.learning.team5shopstrumenti.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, String> {
+}
