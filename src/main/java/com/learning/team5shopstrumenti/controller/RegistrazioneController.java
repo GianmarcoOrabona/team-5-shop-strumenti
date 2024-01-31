09136f7fdc58e7a41d70e32d26ea72600f6f18bc
LuigiantonioIacomino <iacominoluigi54@gmail.com>
2024-01-31 13:30:40 +0100

    modifiche registrazione

---
+++ src/main/java/com/learning/team5shopstrumenti/controller/RegistrazioneController.java
@@ -0,0 +1,36 @@
+package com.learning.team5shopstrumenti.controller;
+
+import com.learning.team5shopstrumenti.interfaccie.UserRepository;
+import com.learning.team5shopstrumenti.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/registrazione")
+public class RegistrazioneController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public String index() {
+        return "login/registrati";
+    }
+
+    @PostMapping
+    public String signUp(@ModelAttribute User formUser) {
+        User user=new User();
+        user.setFirstName(formUser.getFirstName());
+        user.setLastName(formUser.getLastName());
+        user.setEmail(formUser.getEmail());
+        user.setPassword(formUser.getPassword());
+        userRepository.save(user);
+        return "redirect:/strumenti";
+
+    }
+
+}
