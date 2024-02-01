4dd82cce0eaee8dc223edd3ad715642ce3b4ecaa
Gabriele02 <gagag@g,ail.com>
2024-02-01 13:10:08 +0100

    da aggiustare

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -33,3 +33,46 @@ if(validateForm() ){
    alert("Non tutti i campi sono stati compilati")
 }
 })
+
+
+function validateForm() {
+ var name = document.getElementById("nome").value;
+ var cognome = document.getElementById("cognome").value;
+ var citta = document.getElementById("citta").value;
+ var indirizzo = document.getElementById("indirizzo").value;
+ var cap = document.getElementById("cap").value;
+ if (name != "" && cognome != "" && citta != "" && indirizzo != "" && cap != ""){
+     alert("Registrazione andata a buon fine");
+     return false;
+   }
+   return true;
+}
+
+
+document.getElementById("btn").addEventListener('click', function(e){
+if(validateForm() ){
+   e.preventDefault();
+   alert("Non tutti i campi sono stati compilati")
+}
+})
+
+
+function validateFormVendita() {
+ var name = document.getElementById("fname").value;
+ var cognome = document.getElementById("lname").value;
+ var email = document.getElementById("email").value;
+ var indirizzo = document.getElementById("address").value;
+ var citta = document.getElementById("city").value;
+ if (name != "" && cognome != "" && email != "" && indirizzo != "" && city != ""){
+     alert("Aquisto andato a buon fine");
+     return false;
+   }
+   return true;
+}
+
+document.getElementById("btnForm").addEventListener('click', function(e){
+if(validateFormVendita() ){
+   e.preventDefault();
+   alert("Non tutti i campi sono stati compilati")
+}
+})
