43a831ed163244455190a57f530b4af7704c29f4
Gabriele02 <gagag@g,ail.com>
2024-02-01 12:25:39 +0100

    Verifica seulla form di registrazione

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -9,9 +9,27 @@ function validatePassword() {
 }
 
 document.getElementById("btn").addEventListener('click', function(e){
-//e.preventDefault();
-//alert("ciao")
 if(!validatePassword() ){
    e.preventDefault();
+}
+})
+
+function validateForm() {
+ var name = document.getElementById("nome").value;
+ var cognome = document.getElementById("cognome").value;
+ var citta = document.getElementById("citta").value;
+ var indirizzo = document.getElementById("indirizzo").value;
+ var cap = document.getElementById("cap").value;
+ if (name != "" && cognome != "" && citta != "" && indirizzo != "" && cap != ""){
+     alert("Registrazione andata a buon fine");
+     return false;
+   }
+   return true;
+}
+
+document.getElementById("btn").addEventListener('click', function(e){
+if(validateForm() ){
+   e.preventDefault();
+   alert("Non tutti i campi sono stati compilati")
 }
 })
